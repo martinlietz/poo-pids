bc22877e1170457f62d4e3a0e3c4990148dc558d
Martin Lietz <martin.lietz@messer-br.com>
2021-03-15 22:23:59 -0300

    deixar bonitotinho

--- monitor.py
+++ monitor.py
@@ -6,29 +6,29 @@ class monitor:
     _volume:int = 10 # que pode receber de 0 a 100
     _ligado:bool = False
 
-    def __init__(self):
-        pass
+    def __init__(self,horizontal: int=1024, vertical: int=1280):
+        self.liga()
+        self.alteraResolucao(horizontal, vertical)
     
     def aumentaVolume(self):
-        #  possibilita aumentar o volume em incrementos de 5
+        '''possibilita aumentar o volume em incrementos de 5'''
         self._volume = 100 if (self._volume + 5 > 100 and self._ligado) else self._volume + 5
     
     def diminuiVolume(self):
-        # possibilita aumentar o volume em incrementos de 5
+        '''possibilita aumentar o volume em incrementos de 5'''
         self._volume = 0 if (self._volume - 5 < 0 and self._ligado) else self._volume - 5
     
     def liga(self):
-        # liga o monitor
+        '''liga o monitor'''
         self._ligado =  True
     def desliga(self):
-        # desliga o monitor
+        '''desliga o monitor'''
         self._ligado =  False
     def alteraResolucao(self, horizontal: int, vertical: int):
-        # muda a resolução atual, não pode ser superior à resolução máxima horizontal ou vertical
+        '''muda a resolução atual, não pode ser superior à resolução máxima horizontal ou vertical'''
         self._resolucao_horizontal_atual = self._resolucao_maxima_horizontal if horizontal > self._resolucao_maxima_horizontal and self._ligado else horizontal
         self._resolucao_vertical_atual = self._resolucao_maxima_vertical if vertical > self._resolucao_maxima_vertical and self._ligado else vertical
 
 if __name__ == "__main__":
     n = monitor()
-    n.liga()
     n.aumentaVolume()
